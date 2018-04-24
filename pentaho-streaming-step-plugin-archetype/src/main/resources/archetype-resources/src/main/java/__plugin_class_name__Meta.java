4cb439f0bc54d1ff2cb8fe4bfac852a9c1de2bfe
Kurtis Walker <kwalker@pentaho.com>
2018-04-24 09:57:57 -0400

    [BACKLOG-21199] - creating archetype for a streaming plugin (#8)

---
+++ pentaho-streaming-step-plugin-archetype/src/main/resources/archetype-resources/src/main/java/__plugin_class_name__Meta.java
@@ -0,0 +1,75 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.pentaho.di.core.annotations.Step;
+import org.pentaho.di.core.injection.Injection;
+import org.pentaho.di.core.injection.InjectionSupported;
+import org.pentaho.di.core.row.RowMeta;
+import org.pentaho.di.core.row.value.ValueMetaString;
+import org.pentaho.di.core.variables.VariableSpace;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepInterface;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.di.trans.streaming.common.BaseStreamStepMeta;
+
+@Step( id = "${plugin_class_name}", image = "${plugin_class_name}.svg", name = "${plugin_name}",
+  description = "${plugin_description}", categoryDescription = "Streaming" )
+@InjectionSupported( localizationPrefix = "FileStreamMeta.Injection." )
+public class ${plugin_class_name}Meta extends BaseStreamStepMeta implements StepMetaInterface, Cloneable {
+
+  private static Class<?> PKG = ${plugin_class_name}.class; // for i18n purposes, needed by Translator2!!   ${symbol_dollar}NON-NLS-1${symbol_dollar}
+  public static final String CONNECTION_URL = "connectionUrl";
+
+
+  //use injection annotation to automagically get load/save behavior
+  //example
+  @Injection( name = CONNECTION_URL )
+  public String connectionUrl;
+
+  public ${plugin_class_name}Meta() {
+    super();
+  }
+
+  public Object clone() {
+    Object retval = super.clone();
+    return retval;
+  }
+
+  public void setDefault() {
+    //todo: set defaults for all fields
+  }
+
+  @Override
+  public RowMeta getRowMeta( String origin, VariableSpace space ) {
+    RowMeta rowMeta = new RowMeta();
+    rowMeta.addValueMeta( new ValueMetaString( "line" ) );
+    return rowMeta;
+  }
+
+
+  public StepInterface getStep( StepMeta stepMeta, StepDataInterface stepDataInterface, int cnr, TransMeta tr,
+                                Trans trans ) {
+    return new ${plugin_class_name}( stepMeta, stepDataInterface, cnr, tr, trans );
+  }
+
+  public StepDataInterface getStepData() {
+    return new ${plugin_class_name}Data();
+  }
+
+  public String getDialogClassName() {
+    return "${package}.${plugin_class_name}Dialog";
+  }
+
+  public String getConnectionUrl() {
+    return connectionUrl;
+  }
+
+  public void setConnectionUrl( String connectionUrl ) {
+    this.connectionUrl = connectionUrl;
+  }
+}
---
+++ pentaho-streaming-step-plugin-archetype/src/main/resources/archetype-resources/src/main/java/__plugin_class_name__Source.java
@@ -0,0 +1,59 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.pentaho.di.core.logging.LogChannel;
+import org.pentaho.di.core.logging.LogChannelInterface;
+import org.pentaho.di.trans.streaming.common.BlockingQueueStreamSource;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static java.util.Collections.singletonList;
+
+/**
+ * A simple example implementation of StreamSource which streams rows from a specified file to an iterable.
+ * <p>
+ * Note that this class is strictly meant as an example and not intended for real use. It uses a simplistic strategy of
+ * leaving a BufferedReader open in order to load rows as they come in, without real consideration of error conditions.
+ */
+public class ${plugin_class_name}Source extends BlockingQueueStreamSource<List<Object>> {
+
+  private static Class<?> PKG = ${plugin_class_name}.class; // for i18n purposes, needed by Translator2!!   ${symbol_dollar}NON-NLS-1${symbol_dollar}
+  private final ${plugin_class_name}Meta streamMeta;
+
+  private LogChannelInterface logChannel = new LogChannel( this );
+  private final ExecutorService executorService = Executors.newCachedThreadPool();
+  private Future<?> future;
+
+  public ${plugin_class_name}Source( ${plugin_class_name}Meta streamMeta, ${plugin_class_name} streamStep ) {
+    super( streamStep );
+    this.streamMeta = streamMeta;
+  }
+
+
+  @Override public void open() {
+    if ( future != null ) {
+      logChannel.logError( "open() called more than once" );
+      return;
+    }
+    future = executorService.submit( this::readLoop );
+  }
+
+  @Override public void close() {
+    super.close();
+    future.cancel( true );
+  }
+
+  private void readLoop() {
+    //this method should continuously call acceptRows with the next row retrieved from your streaming source
+    //example:
+    while ( true ) {
+      acceptRows( singletonList( singletonList( "some value read from your source" ) ) );
+    }
+  }
+
+}
